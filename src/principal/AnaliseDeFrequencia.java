9d6bf6ebdb510283246f710b5381a139337b70ee
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2020-11-03 17:58:49 -0300

    identificando pontuacao e contando total de caracteres

--- src/principal/AnaliseDeFrequencia.java
+++ src/principal/AnaliseDeFrequencia.java
@@ -6,10 +6,11 @@ public class AnaliseDeFrequencia {
 
 	public static void main(String[] args) throws IOException {
 		String path = "C:\\Users\\Vini\\Documents\\workspace-spring-tool-suite-4-4.7.1.RELASE\\analiseDeFrequencia\\src\\";
-		String arquivo = (path+"teste.txt");
+		String arquivo = (path+"teste2.txt");
 		
 		ManipuladorDeArquivo.leitor(arquivo);
-		
+
+
 	}
 
 }
--- src/principal/ManipuladorDeArquivo.java
+++ src/principal/ManipuladorDeArquivo.java
@@ -1,17 +1,14 @@
 package principal;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
 
 public class ManipuladorDeArquivo {
+	
 
 	public static void leitor(String path) throws IOException {
 
@@ -21,12 +18,32 @@ public class ManipuladorDeArquivo {
 
 		// loop que lê e imprime todas as linhas do arquivo
 		String linha = myBuffer.readLine();
+		int qtdCaracteres =0;
+		
+		for(int i = 0 ; i < linha.toUpperCase().length(); i++) {
+			char caracter = linha.charAt(i);
+			qtdCaracteres+=linha.length();
+			if(caracter == ' '||caracter == '!'||caracter=='?'||caracter==','||caracter=='.'||
+					caracter==';'||caracter==':'||caracter=='-'||caracter=='('||caracter==')') {
+				System.out.println("pontuacao");
+			} else if (caracter != ' '){
+				for(int j = 0; j < linha.toUpperCase().length() ;j++) { 
+				char l = linha.charAt(j);
+					if (caracter == l){
+						System.out.println(l);
+					}
+				}
+			}
+		}
+		
 
 		while (linha != null) {
+
 			System.out.println(linha);
 			linha = myBuffer.readLine();
-		}
-
+			}
+		
+		System.out.println("O texto possui "+qtdCaracteres+" caracteres ao todo");
 		myBuffer.close();
 
 	}
